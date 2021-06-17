ce2a638c76762f41b32124fc0bb28413dada0174
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2021-06-17 13:49:12 -0500

    Dao package created. Added FolderDao and FolderRepository.

---
+++ app/src/main/java/com/example/note_tech_geeks_android/dao/FolderDao.java
@@ -0,0 +1,32 @@
+package com.example.note_tech_geeks_android.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.note_tech_geeks_android.models.Folder;
+import com.example.note_tech_geeks_android.models.FolderWithNotes;
+
+import java.util.List;
+
+@Dao
+public interface FolderDao {
+    @Query("SELECT * FROM folders")
+    LiveData<List<FolderWithNotes>> loadFolderList();
+
+    @Query("SELECT COUNT(folderId) FROM notes WHERE folderId=:id")
+    int getFolderNotesCount(int id);
+
+    @Insert
+    void insertFolder(Folder folder);
+
+    @Update
+    void updateFolder(Folder folder);
+
+    @Delete
+    void deleteFolder(Folder folder);
+}
+
---
+++ app/src/main/java/com/example/note_tech_geeks_android/dao/FolderRepository.java
@@ -0,0 +1,42 @@
+package com.example.note_tech_geeks_android.dao;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.note_tech_geeks_android.db.NoteAppDatabase;
+import com.example.note_tech_geeks_android.models.Folder;
+import com.example.note_tech_geeks_android.models.FolderWithNotes;
+
+import java.util.List;
+
+public class FolderRepository {
+    private final FolderDao mFolderDao;
+    private final LiveData<List<FolderWithNotes>> allFolderWithNotes;
+
+    public FolderRepository(Application application) {
+        NoteAppDatabase db = NoteAppDatabase.getInstance(application);
+        mFolderDao = db.folderDao();
+        allFolderWithNotes = mFolderDao.loadFolderList();
+    }
+
+    public int getFolderNotesCount(int id) {
+        return mFolderDao.getFolderNotesCount(id);
+    }
+
+    public LiveData<List<FolderWithNotes>> getAllFolderWithNotes() {
+        return allFolderWithNotes;
+    }
+
+    public void insert(Folder folder) {
+        NoteAppDatabase.databaseWriteExecutor.execute(() -> mFolderDao.insertFolder(folder));
+    }
+
+    public void update(Folder folder) {
+        NoteAppDatabase.databaseWriteExecutor.execute(() -> mFolderDao.updateFolder(folder));
+    }
+
+    public void delete(Folder folder) {
+        NoteAppDatabase.databaseWriteExecutor.execute(() -> mFolderDao.deleteFolder(folder));
+    }
+}
